8e7003538425e9280c6e3561628b96b0e0cb882f
Ivan Pichurin <ivan.pichurin@gmail.com>
2023-10-05 20:29:30 +0300

    9 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,41 +3,39 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+       System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
+       WebDriver driver = new ChromeDriver();
+       //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+       driver.manage().window().maximize();
 
-System.setProperty("webdriver.gecko.driver","C:\\webdriver\\geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
-      /*  driver.get("http://ya.ru");
-        WebElement we = driver.findElement(By.id("text"));
-        we.sendKeys("Selenium webdriver");
-        WebElement b = driver.findElement(By.xpath("//button[@type='submit']"));
-        b.click();
-*/
-int a;
-   int aa;
-            driver.get("https://demoqa.com/droppable");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            WebElement source = driver.findElement(By.xpath("//div[@id='draggable']"));
-           WebElement target = driver.findElement(By.xpath("//div[@id='droppable']"));
+       driver.get("https://demoqa.com/text-box");
 
+       WebElement userNameInput = driver.findElement(By.xpath("//input[@id='userName']"));
 
+       userNameInput.sendKeys("Ivan");
 
-            Actions builder = new Actions(driver);
-          //  builder.dragAndDrop(source, target).perform();
-        builder.dragAndDropBy(source,300,100).build().perform();
+       WebElement email = driver.findElement(By.xpath("//input[@id='userEmail']"));
+
+       email.sendKeys("ivan.pichurin@gmail.com");
+
+        WebElement CurrentAddress = driver.findElement(By.xpath("//input[@id='currenAddress']"));
+
+        CurrentAddress.sendKeys("123");
 
 
+        WebElement PermanentAddress = driver.findElement(By.xpath("//input[@id='permanentAddress']"));
 
+        PermanentAddress.sendKeys("ivan.pichurin@gmail.com");
 
-
+        WebElement button = driver.findElement(By.xpath("//button[@id='btn btn-primary']"));
+button.click();
     }
 }
